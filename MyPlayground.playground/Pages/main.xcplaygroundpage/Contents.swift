7f44a8f1e84be3e6f479d5baf79be5c554404ea2
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-07-05 14:18:32 -0400

    Functioning around

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,31 +13,53 @@
  */
 // write your code here
 
+func helloWorld() {
+    
+    print("Hello, world!")
+    
+}
 
-
+helloWorld()
 
 /*: question2
  ### 2. Write your own function in which you declare a constant inside the function's body and then print that constant to the console. Call this function to see your string printed to the playground console.
  */
 // write your code here
 
+func newFunction () {
+    
+    let favoriteProducer = "J DILLA"
+    print(favoriteProducer)
+    
+}
 
-
-
+newFunction()
 /*: question3
  ### 3. Write a function that takes a person's name as an argument and prints a greeting to the console. Call it several times with different arguments. What do you think you'll see in the console?
  */
 // write your code here
 
+func greeting(personName: String) {
+    
+    print("What's going on \(personName)? How's your day?")
+    
+}
 
-
-
+greeting("Claire")
+greeting("Alex")
+greeting("John")
 /*: question4
  ### 4. Now call the function you wrote in Question 3 using a variable or constant instead of a string literal. What do you expect to see in the console? Try passing in a _variable_ you declared (using `var`) as an argument. Then change that variable's value and call your function again. What do you see in the console?
  */
 // write your code here
 
+let classmate = "Max"
+greeting(classmate)
 
+var newFriend = "Colin"
+newFriend = "Jordan"
+
+greeting(newFriend)
 
 
 /*: question5
@@ -45,8 +67,18 @@
  */
 // write your code here
 
+func customFunction () {
+    
+    var anyType = 4
+    print(anyType)
+    
+    anyType = 6
+    print(anyType)
+    
+}
 
-
+customFunction()
+customFunction()
 
 /*:
  [Solution](solution)
